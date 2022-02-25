d50cd33f2cbd9b0b33f9673f3999db603c7d2edc
allanzaparoli <allanzaparoli@gmail.com>
2022-02-25 20:13:43 -0300

    atualizacao-Allan

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 const btnLogin = document.querySelector('.btn-header');
 const inputEmail = document.querySelector('#email');
-const inputPassword = document.querySelector('#password')
+const inputPassword = document.querySelector('#password');
 
 function login() {
     const email = inputEmail.value;
-    const password = inputPassword.value
-     if (email === "tryber@teste.com" && password === "123456"){
+    const password = inputPassword.value;
+     if (email === "tryber@teste.com" && password === "123456") {
     alert('Olá, Tryber!');
   }
-  }
-
-//  btnLogin.addEventListener("click",login)
-const email = '';
+}
+btnLogin.addEventListener("click",login);
