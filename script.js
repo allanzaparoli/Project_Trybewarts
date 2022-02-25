4e99f76dce9c51a0043a85d6814c42244562297b
NetoGC <antonio.netogc0@gmail.com>
2022-02-25 17:10:03 -0300

    alteraçoes bobas script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-let btnLogin = document.querySelector('.btn-header');
-let inputEmail = document.querySelector('#email');
-let inputPassword = document.querySelector('#password')
+const btnLogin = document.querySelector('.btn-header');
+const inputEmail = document.querySelector('#email');
+const inputPassword = document.querySelector('#password')
 
-function login(e){
-    let email = inputEmail.value;
-    let password = inputPassword.value
-     if(email == "tryber@teste.com" && password == "123456"){
+function login() {
+    const email = inputEmail.value;
+    const password = inputPassword.value
+     if (email === "tryber@teste.com" && password === "123456"){
     alert('Olá, Tryber!');
-    }
- console.log(email);   
-}
+  }
+  }
 
  btnLogin.addEventListener("click",login) 
