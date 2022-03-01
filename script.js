cd4525acd33c904b5fa9061dae469fa45ee94cca
allanzaparoli <allanzaparoli@gmail.com>
2022-03-01 20:44:37 -0300

    finalização dos requisitos

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const textArea = document.querySelector('#textarea');
 const counter = document.querySelector('#counter');
 const submitBtn = document.querySelector('#submit-btn');
 const agreement = document.querySelector('#agreement');
-
-
+const dadosForm = document.querySelector('#evaluation-form');
+const vlrMateria = [];
 submitBtn.disabled = true;
 
 function login() {
@@ -35,11 +35,27 @@ function submitPrevent() {
     submitBtn.disabled = true;
   }
 }
-
 btnLogin.addEventListener('click', login);
 textArea.addEventListener('keyup', counteChars);
 agreement.addEventListener('click', submitPrevent);
 
-function enviar(event) {
+function chamarEvento(event) {
+  event.preventDefault();
+  const nome = document.getElementById('input-name').value;
+  const sobrenome = document.getElementById('input-lastname').value;
+  const nomeCompleto = `${nome} ${sobrenome}`;
+  const email = document.getElementById('input-email').value;
+  const casa = document.getElementById('house').value;
+  const familia = document.querySelector('.family:checked').value;
+  const avaliacao = document.querySelector('.rate:checked').value;
+  const comentario = document.getElementById('textarea').value;
+  const materia = document.querySelectorAll('.subject:checked');
+  for (let index = 0; index < materia.length; index += 1) {
+    vlrMateria.push(materia[index].value);
+  }
+  const printMaterias = vlrMateria.join(', ');
+  dadosForm.innerHTML = `<p>Nome: ${nomeCompleto}</p> <p>Email: ${email}</p> <p>Casa: ${casa}</p> 
+  <p>Família: ${familia}</p> <p>Matérias: ${printMaterias}</p> <p>Avaliação: ${avaliacao}</p> 
+  <p>Observações: ${comentario}</p>`;
 }
-submitBtn.addEventListener('click', enviar);
+submitBtn.addEventListener('click', chamarEvento);
